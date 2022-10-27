8f6663129f0351f9d67d5fb229de300908fb9dbc
bazz <clement.f.bazz@gmail.com>
2022-10-27 20:28:35 +0100

    added text property

--- src/appData.js
+++ src/appData.js
@@ -1,4 +1,4 @@
-const uiData = [{url:`https://training.zuri.team/`, id:`btn__zuri`}, {url:`http://books.zuri.team`, id:`books`}, {url:` http://books.zuri.team/python-for-beginners?ref_id=<yourslackname>`, id:`book_python`},{url:`https://background.zuri.team`, id:`pitch`},{url:`https://books.zuri.team/design-rules`, id:`book__design`}]
+const uiData = [{url:`https://training.zuri.team/`, id:`btn__zuri`, text:'Zuri Team'}, {url:`http://books.zuri.team`, id:`books`, text:'Zuri Books'}, {url:` http://books.zuri.team/python-for-beginners?ref_id=<yourslackname>`, id:`book_python`, text:'Python Books'},{url:`https://background.zuri.team`, id:`pitch`, text:'Background Check for Coders'},{url:`https://books.zuri.team/design-rules`, id:`book__design`, text:'Design Books'}]
 
 const userDetails = {
         name: 'Clement Bazuaye',
