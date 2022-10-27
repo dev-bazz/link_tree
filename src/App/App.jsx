98d22adfc1c3a7cf4262747b8718c020995db2c9
bazz <clement.f.bazz@gmail.com>
2022-10-27 19:10:23 +0100

    importer profile and footer component

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,7 +1,13 @@
+import Footer from "./Footer";
+import ProfileDetails from "./ProfileDetails";
 
 
 export default function App() {
   return (
-    <div>App</div>
+    <div>App
+        <ProfileDetails />
+        {/* Links Components */}
+        <Footer />
+    </div>
   )
 }
