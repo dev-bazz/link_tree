3572abf81630f1c02f482845fdfbe614626a8014
bazz <clement.f.bazz@gmail.com>
2022-10-27 20:28:13 +0100

    created html structure

--- src/App/LinkButton.jsx
+++ src/App/LinkButton.jsx
@@ -1,7 +1,14 @@
 
 
-export default function LinkButton() {
+export default function LinkButton({ url, id, text }) {
   return (
-    <div>LinkButton</div>
+    <li className="btn-link">
+      <a href={url} id={id} className='btn-link__a'>{text}</a>
+    </li>
   )
+}
+LinkButton.defaultProps = {
+  url: `https://training.zuri.team/`,
+  id: `btn__zuri`,
+  text: 'Default'
 }
