92595fa1376b9985ef80fe348780fa0ac4bd3351
bazz <clement.f.bazz@gmail.com>
2022-10-27 19:41:05 +0100

    changed wrapper to profile in class

--- src/App/ProfileDetails.jsx
+++ src/App/ProfileDetails.jsx
@@ -3,7 +3,7 @@
 export default function ProfileDetails() {
   return (
     <header>
-<div className="wrapper">
+<div className="profile">
   <div className="profile__wrapper">
     <img src="https://avatars.githubusercontent.com/u/67967265?v=4" alt="" className="profile__img" />
   </div>
