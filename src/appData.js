a225b752771441274c15604ebe15ba451b58c909
bazz <clement.f.bazz@gmail.com>
2022-10-27 19:09:37 +0100

    Added project requirement data and user data

---
+++ src/appData.js
@@ -0,0 +1,6 @@
+const uiData = [{url:`https://training.zuri.team/`, id:`btn__zuri`}, {url:`http://books.zuri.team`, id:`books`}, {url:` http://books.zuri.team/python-for-beginners?ref_id=<yourslackname>`, id:`book_python`},{url:`https://background.zuri.team`, id:`pitch`},{url:`https://books.zuri.team/design-rules`, id:`book__design`}]
+
+const userDetails = {
+        name: 'Clement Bazuaye',
+        email: 'bazuaye50@gmail.com'
+}
