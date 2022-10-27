5e3abfdbc9003bdb228d6de4e80536abf48cd519
bazz <clement.f.bazz@gmail.com>
2022-10-27 19:18:15 +0100

    added basic UI Information

--- src/App/ProfileDetails.jsx
+++ src/App/ProfileDetails.jsx
@@ -2,6 +2,15 @@
 
 export default function ProfileDetails() {
   return (
-    <div>ProfileDetails</div>
+    <header>
+<div className="wrapper">
+  <div className="profile__wrapper">
+    <img src="https://avatars.githubusercontent.com/u/67967265?v=4" alt="" className="profile__img" />
+  </div>
+  <h1 className="profile__name">
+    User Clement
+  </h1>
+</div>
+    </header>
   )
 }
