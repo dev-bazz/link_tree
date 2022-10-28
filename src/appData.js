8a573087f3f1b1a530bae3816f11c735863aba1d
bazz <clement.f.bazz@gmail.com>
2022-10-28 05:47:22 +0100

    Proper Data format

--- src/appData.js
+++ src/appData.js
@@ -1,4 +1,4 @@
-export const uiData = [{ url: `https://training.zuri.team/`, id: `btn__zuri`, text: 'Zuri Team' }, { url: `http://books.zuri.team`, id: `books`, text: 'Zuri Books' }, { url: ` http://books.zuri.team/python-for-beginners?ref_id=<yourslackname>`, id: `book_python`, text: 'Python Books' }, { url: `https://background.zuri.team`, id: `pitch`, text: 'Background Check for Coders' }, { url: `https://books.zuri.team/design-rules`, id: `book__design`, text: 'Design Books' }]
+export const uiData = [{url:'https://twitter.com/bazz_ng',id:'twitter', text:'Twitter Link'},{ url: `https://training.zuri.team/`, id: `btn__zuri`, text: 'Zuri Team' }, { url: `http://books.zuri.team`, id: `books`, text: 'Zuri Books' }, { url: ` http://books.zuri.team/python-for-beginners?ref_id=<yourslackname>`, id: `book_python`, text: 'Python Books' }, { url: `https://background.zuri.team`, id: `pitch`, text: 'Background Check for Coders' }, { url: `https://books.zuri.team/design-rules`, id: `book__design`, text: 'Design Books' }]
 
 export const userDetails = {
         name: 'Clement Bazuaye',
