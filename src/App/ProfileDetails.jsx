a8bcbddcfa3d4006f79075051074ce81bd7a5df4
bazz <clement.f.bazz@gmail.com>
2022-10-28 06:14:06 +0100

    updating

--- src/App/ProfileDetails.jsx
+++ src/App/ProfileDetails.jsx
@@ -5,9 +5,9 @@ export default function ProfileDetails({ user }) {
     <header>
       <div className="profile">
         <div className="profile__wrapper">
-          <img src="https://avatars.githubusercontent.com/u/67967265?v=4" alt="" className="profile__img" />
+          <img src="https://avatars.githubusercontent.com/u/67967265?v=4" alt="" className="profile__img" id="profile_img"/>
         </div>
-        <h1 className="profile__name">
+        <h1 className="profile__name" id="twitter">
           {user}
         </h1>
       </div>
