08715dd5d94a0e633d9a64f1aeaa109f20e5b547
bazz <clement.f.bazz@gmail.com>
2022-10-28 16:26:32 +0100

    Added Footer JSX

--- src/App/Footer.jsx
+++ src/App/Footer.jsx
@@ -1,7 +1,14 @@
-
+import zuriInternshipLogo from '../img/Zuri.Internship_Logo.svg';
+import i4g from "../img/I4G.png";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer className='footer' >
+      <div className="footer___wrapper">
+        <img src={zuriInternshipLogo} alt="Zuri Internship Logo" className='footer__img--left' />
+        <p className='footer__text' >HNG Internship 9 Frontend Task</p>
+        <img src={i4g} alt="Ingressive for Good Logo" className='footer__img--right' />
+      </div>
+    </footer>
   )
 }
