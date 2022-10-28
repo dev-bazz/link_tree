b2eb407d06e99b07d240739b606a1fc2f7912e39
bazz <clement.f.bazz@gmail.com>
2022-10-28 05:47:41 +0100

    Updating

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,15 +1,29 @@
+import { useState } from "react";
 import Footer from "./Footer";
 import LinkButton from "./LinkButton";
 import ProfileDetails from "./ProfileDetails";
-
+import {uiData, userDetails} from "../appData"
 
 export default function App() {
+
+const [linkData] = useState(uiData)
+const [{name}] = useState(userDetails)
+
   return (
     <div>App
-        <ProfileDetails />
-        {/* Links Components */}
-        <LinkButton />
-        <Footer />
+      <ProfileDetails user={name}/>
+      {/* Links Components */}
+      <div className="link-wrapper">
+      {linkData.map((link, index)=>{
+        const {url, text,id} = link;
+        return <LinkButton 
+        url={url}
+        text={text}
+        id={id}
+         key={index} />
+      })}
+      </div>
+      <Footer />
     </div>
   )
 }
--- src/App/ProfileDetails.jsx
+++ src/App/ProfileDetails.jsx
@@ -1,16 +1,19 @@
 
 
-export default function ProfileDetails() {
+export default function ProfileDetails({ user }) {
   return (
     <header>
-<div className="profile">
-  <div className="profile__wrapper">
-    <img src="https://avatars.githubusercontent.com/u/67967265?v=4" alt="" className="profile__img" />
-  </div>
-  <h1 className="profile__name">
-    User:Clement
-  </h1>
-</div>
+      <div className="profile">
+        <div className="profile__wrapper">
+          <img src="https://avatars.githubusercontent.com/u/67967265?v=4" alt="" className="profile__img" />
+        </div>
+        <h1 className="profile__name">
+          {user}
+        </h1>
+      </div>
     </header>
   )
+}
+ProfileDetails.defaultProps = {
+  user: 'Clement'
 }
