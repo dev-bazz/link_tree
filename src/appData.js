c26f5e627285e0687d632b7d4be59ffcc1f06b61
bazz <clement.f.bazz@gmail.com>
2022-10-28 04:55:57 +0100

    added export to file

--- src/appData.js
+++ src/appData.js
@@ -1,6 +1,8 @@
-const uiData = [{url:`https://training.zuri.team/`, id:`btn__zuri`, text:'Zuri Team'}, {url:`http://books.zuri.team`, id:`books`, text:'Zuri Books'}, {url:` http://books.zuri.team/python-for-beginners?ref_id=<yourslackname>`, id:`book_python`, text:'Python Books'},{url:`https://background.zuri.team`, id:`pitch`, text:'Background Check for Coders'},{url:`https://books.zuri.team/design-rules`, id:`book__design`, text:'Design Books'}]
+export const uiData = [{ url: `https://training.zuri.team/`, id: `btn__zuri`, text: 'Zuri Team' }, { url: `http://books.zuri.team`, id: `books`, text: 'Zuri Books' }, { url: ` http://books.zuri.team/python-for-beginners?ref_id=<yourslackname>`, id: `book_python`, text: 'Python Books' }, { url: `https://background.zuri.team`, id: `pitch`, text: 'Background Check for Coders' }, { url: `https://books.zuri.team/design-rules`, id: `book__design`, text: 'Design Books' }]
 
-const userDetails = {
+export const userDetails = {
         name: 'Clement Bazuaye',
         email: 'bazuaye50@gmail.com'
 }
+
+
