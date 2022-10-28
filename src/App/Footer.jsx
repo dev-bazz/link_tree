ab540e44ec0beb49d35d3f539bcb1114fcad5abf
bazz <clement.f.bazz@gmail.com>
2022-10-28 20:44:21 +0100

    Created JSX footer

--- src/App/Footer.jsx
+++ src/App/Footer.jsx
@@ -4,10 +4,14 @@ import i4g from "../img/I4G.png";
 export default function Footer() {
   return (
     <footer className='footer' >
-      <div className="footer___wrapper">
+      <div className="footer__wrapper">
+        <a href="https://internship.zuri.team/">
         <img src={zuriInternshipLogo} alt="Zuri Internship Logo" className='footer__img--left' />
+        </a>
         <p className='footer__text' >HNG Internship 9 Frontend Task</p>
+        <a href="https://ingressive.org/">
         <img src={i4g} alt="Ingressive for Good Logo" className='footer__img--right' />
+        </a>
       </div>
     </footer>
   )
