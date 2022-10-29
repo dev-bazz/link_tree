22b703ddbfe17e63cf38121f5bdf4e83d6d56850
bazz <clement.f.bazz@gmail.com>
2022-10-29 06:04:26 +0100

    Made Footer to be colum on small devices

--- src/App/ProfileDetails.jsx
+++ src/App/ProfileDetails.jsx
@@ -6,12 +6,12 @@ export default function ProfileDetails({ user }) {
   return (
     <header>
       <div className="profile">
-        
+
         <div className="profile__wrapper">
           <div className="hovered">
-          <img src={change} alt="change Image" />
-        </div>
-          <img src="https://avatars.githubusercontent.com/u/67967265?v=4" alt="" className="profile__img" id="profile_img"/>
+            <img src={change} alt="change Image" />
+          </div>
+          <img src="https://avatars.githubusercontent.com/u/67967265?v=4" alt="" className="profile__img" id="profile_img" />
         </div>
         <h1 className="profile__name" id="twitter">
           {user}
