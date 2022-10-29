59d9bbbf2f15ef9911f8ce2255411f2fc70f3ece
bazz <clement.f.bazz@gmail.com>
2022-10-29 03:11:14 +0100

    Update

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -2,29 +2,30 @@ import { useState } from "react";
 import Footer from "./Footer";
 import LinkButton from "./LinkButton";
 import ProfileDetails from "./ProfileDetails";
-import {uiData, userDetails} from "../appData";
+import { uiData, userDetails } from "../appData";
 import slack from "../img/slack.svg";
 import gitHub from "../img/Social icon.svg";
 
 export default function App() {
 
-const [linkData] = useState(uiData)
-const [{name}] = useState(userDetails)
+  const [linkData] = useState(uiData)
+  const [{ name }] = useState(userDetails)
 
   return (
-    <div>App
-      <ProfileDetails user={name}/>
+    <div>
+      <ProfileDetails user={name} />
       {/* Links Components */}
       <ul className="link-wrapper">
-      {linkData.map((link, index)=>{
-        const {url, text,id} = link;
-        return <LinkButton 
-        url={url}
-        text={text}
-        id={id}
-         key={index} />
-      })}
-      
+        {linkData.map((link, index) => {
+          const { url, text, id, title } = link;
+          return <LinkButton
+            url={url}
+            text={text}
+            id={id}
+            key={index}
+            title={title} />
+        })}
+
       </ul>
       <div className="social">
         <a href="https://slack.com/"><img src={slack} alt="slack Link" /></a>
--- src/App/ProfileDetails.jsx
+++ src/App/ProfileDetails.jsx
@@ -1,15 +1,26 @@
-
+import shareLD from "../img/Icon.svg"
+import shareSd from "../img/Icon(1).svg"
+import change from "../img/Icon(2).svg"
 
 export default function ProfileDetails({ user }) {
   return (
     <header>
       <div className="profile">
+        
         <div className="profile__wrapper">
+          <div className="hovered">
+          <img src={change} alt="change Image" />
+        </div>
           <img src="https://avatars.githubusercontent.com/u/67967265?v=4" alt="" className="profile__img" id="profile_img"/>
         </div>
         <h1 className="profile__name" id="twitter">
           {user}
         </h1>
+        <div id="slack"></div>
+      </div>
+      <div className="share">
+        <img src={shareLD} alt="share" className="ld" />
+        <img src={shareSd} alt="share" className="sd" />
       </div>
     </header>
   )
