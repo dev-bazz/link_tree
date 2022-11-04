18dddb8d03fcb8dc331e07a64df0a708baded4dd
bazz <clement.f.bazz@gmail.com>
2022-11-04 02:56:02 +0100

    Feat: Implemented Routes and Route inApp.jsx

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,37 +1,15 @@
-import { useState } from "react";
 import Footer from "./Footer";
-import LinkButton from "./LinkButton";
-import ProfileDetails from "./ProfileDetails";
-import { uiData, userDetails } from "../appData";
-import slack from "../img/slack.svg";
-import gitHub from "../img/Social icon.svg";
+import Home from "./Home";
+import {Routes,Route} from "react-router-dom"
+
 
 export default function App() {
-
-  const [linkData] = useState(uiData)
-  const [{ name }] = useState(userDetails)
-
   return (
-    <div>
-      <ProfileDetails user={name} />
-      {/* Links Components */}
-      <ul className="link-wrapper">
-        {linkData.map((link, index) => {
-          const { url, text, id, title } = link;
-          return <LinkButton
-            url={url}
-            text={text}
-            id={id}
-            key={index}
-            title={title} />
-        })}
-
-      </ul>
-      <div className="social">
-        <a href="https://slack.com/"><img src={slack} alt="slack Link" /></a>
-        <a href="https://github.com/dev-bazz/link_tree"><img src={gitHub} alt="github" /></a>
-      </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
       <Footer />
-    </div>
+    </>
   )
 }
