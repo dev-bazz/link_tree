09b9545448364c31a1985c0402739ffdabc75dd6
bazz <clement.f.bazz@gmail.com>
2022-11-04 05:55:26 +0100

    Update: CSS Format

--- src/App/ContactMe.jsx
+++ src/App/ContactMe.jsx
@@ -2,6 +2,9 @@
 
 export default function ContactMe() {
         return (
-                <div>ContactMe</div>
+                <div className="content-container contact-us">
+                        <h1>Contact Me</h1>
+                        <p>Hi there, contact me to ask me about anything you have in mind.</p>
+                </div>
         )
 }
