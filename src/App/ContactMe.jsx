a69cda391ffc535020b85a08980aa93e062bf437
bazz <clement.f.bazz@gmail.com>
2022-11-04 06:50:37 +0100

    Feat: Basic Styling and Input for Contact Page

--- src/App/ContactMe.jsx
+++ src/App/ContactMe.jsx
@@ -5,8 +5,21 @@ export default function ContactMe() {
                 <div className="content-container contact-us">
                         <div className="contact-us__msg">
                                 <h1 className="contact-us__h1">Contact Me</h1>
-                                <p>Hi there, contact me to ask me about anything you have in mind.</p>
+                                <p className="contact-us__p">Hi there, contact me to ask me about anything you have in mind.</p>
                         </div>
+                        <form action="#" className="form">
+                                <div className="form__flex">
+                                        <div className="form__name">
+                                                <label htmlFor="first-name">First Name</label>
+                                                <input type="text" id="first-name" />
+                                        </div>
+                                        <div className="form__name">
+                                                <label htmlFor="last-name">Last Name</label>
+                                                <input type="text" id="last-name" />
+                                        </div>
+                                </div>
+
+                        </form>
                 </div>
         )
 }
