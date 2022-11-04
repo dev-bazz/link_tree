80ae476b7951735a14fd946b46ac908db479c4ec
bazz <clement.f.bazz@gmail.com>
2022-11-04 06:27:47 +0100

    Feat: Style Formating

--- src/App/ContactMe.jsx
+++ src/App/ContactMe.jsx
@@ -3,8 +3,10 @@
 export default function ContactMe() {
         return (
                 <div className="content-container contact-us">
-                        <h1>Contact Me</h1>
-                        <p>Hi there, contact me to ask me about anything you have in mind.</p>
+                        <div className="contact-us__msg">
+                                <h1 className="contact-us__h1">Contact Me</h1>
+                                <p>Hi there, contact me to ask me about anything you have in mind.</p>
+                        </div>
                 </div>
         )
 }
