214bbbf408a710b1cbbe228cdd4b2e79ab4d0249
bazz <clement.f.bazz@gmail.com>
2022-11-04 02:38:36 +0100

    Feat: Wrapped Root Aplication with Web router

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client"
 import App from "./App/App";
 import "./index.css";
+import {BrowserRouter as Router} from "react-router-dom"
 
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 root.render(
         <React.StrictMode>
+                <Router>
                 <App />
+                </Router>
         </React.StrictMode>
 )
