c25bb7ba0e31d08186fd754ba84372e02a2374a9
bazz <clement.f.bazz@gmail.com>
2022-11-05 06:17:22 +0100

    Feat: erro msg

--- src/App/ContactMe.jsx
+++ src/App/ContactMe.jsx
@@ -1,6 +1,29 @@
+import { useReducer } from "react"
+
+const formData = {
+        firstName: '',
+        lastName: '',
+        message: ''
+}
+
+function reducer(currentState, action){
+        switch(action.type){
+                case 'hello':
+                        return {...currentState}
+
+        }
+}
 
 
 export default function ContactMe() {
+
+        const [{ firstName, lastName, message }, dispatch] = useReducer(reducer, formData);
+        function onClickF(){
+                firstName || console.log('Hey this is not working')
+        }
+
+
+
         return (
                 <div className="content-container contact-us">
                         <div className="contact-us__msg">
@@ -10,25 +33,25 @@ export default function ContactMe() {
                         <form action="#" className="form">
                                 <div className="form__flex">
                                         <fieldset className="form__name" >
-                                                <label htmlFor="first_name">First Name</label>
-                                                <input type="text" id="first_name" placeholder="Enter your first name" data-erro="Please Enter  Your FIrst Name" required/>
+                                                <label data-error="You forgot to add your first name" htmlFor="first_name">First Name</label>
+                                                <input type="text" id="first_name" placeholder="Enter your first name" data-erro="Please Enter  Your FIrst Name" required />
                                         </fieldset>
                                         <fieldset className="form__name">
-                                                <label htmlFor="last_name">Last Name</label>
-                                                <input type="text" id="last_name" placeholder="Enter your last name" required />
+                                                <label data-error="No Last Name" htmlFor="last_name">Last Name</label>
+                                                <input data-error="You forgot to put your last name" className="error" type="text" id="last_name" placeholder="Enter your last name" required />
                                         </fieldset>
                                 </div>
                                 <fieldset className="email">
                                         <label htmlFor="email">Email</label>
-                                        <input type="email" id="email" placeholder="yourname@email.com" required />
+                                        <input type="email" onClick={(e)=> onClickF(e)} id="email" placeholder="yourname@email.com" required />
                                 </fieldset>
                                 <fieldset className="message">
                                         <label htmlFor="message">Message</label>
-                                        <textarea name="message" id="message" cols="30" rows="10" placeholder="Send me a message and I'll reply you as soon as possible..." required></textarea>
+                                        <textarea  name="message" id="message" cols="30" rows="10" placeholder="Send me a message and I'll reply you as soon as possible..." required></textarea>
                                 </fieldset>
                                 <fieldset className="check-ox">
                                         <input type="checkbox" name="" id="checkbox" />
-                                        <label htmlFor="">You agree to providing your data to Clement who may contact you.</label>
+                                        <label htmlFor="checkbox">You agree to providing your data to Clement who may contact you.</label>
                                 </fieldset>
                                 <input type="submit" value="Send message" className="form-cta" />
                         </form>
